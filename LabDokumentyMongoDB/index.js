ea1a91285803b60f2d2650c5f3be4e90382d931a
MateuszBonar <mateusz211@vp.pl>
2020-05-09 10:30:05 +0200

    Poprawienie mongoDB

--- LabDokumentyMongoDB/index.js
+++ LabDokumentyMongoDB/index.js
@@ -1,11 +1,10 @@
-const mongo = require('mongodb');
 const prompt = require('prompt-sync')();
-//let animals;
+const mongo = require('mongodb');
+var ObjectId = require('mongodb').ObjectID;
 const client = new mongo.MongoClient('mongodb://localhost:27017', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-
 
 createMenu = () => {
   console.log('Witaj w menu:');
@@ -13,12 +12,13 @@ createMenu = () => {
   console.log('2. Dodaj ');
   console.log('3. Update ');
   console.log('4. Usun ');
-  console.log('5. Wyjscie');
+  console.log('5. Wypisz po ID');
+  console.log('7. Wyjscie');
 };
 
 addAnimal = () => {
-    const animalName = prompt('Imie zwierzatka: ');
-  const ageAnimal = prompt('Wiek zwierzatka: ');
+  const animalName = prompt('Name: ');
+  const ageAnimal = prompt('Age: ');
   animals.insertOne(
     {
       nameAnimal: animalName,
@@ -26,110 +26,63 @@ addAnimal = () => {
     },
     (err) => {
       if (err) {
-        console.log('Nie dodano wpisu');
+        console.log('Error');
       } else {
-        console.log('Wpis dodano');
+        console.log('Dodano!');
       }
     }
   );
 };
-
-connectDatabase = () => {
-  client.connect((err) => {
+deleteAnimal = () => {
+  const animalId = prompt('Id zwierzatka do usuniecia: ');
+  animals.deleteOne({ _id: mongo.ObjectID(animalId) });
+};
+printAnimal = () => {
+  animals.find({}).toArray((err, animalList) => {
     if (err) {
-      console.log('Błąd połączenia');
+      console.log('Bledne zapytanie');
     } else {
-      console.log('Połączenie udane');
-      const db = client.db('test');
-      animals = db.collection('animals');
-
-      // animals.deleteOne({_id: mongo.ObjectID()});
-
-      //    animals.find({}).toArray((err,animalList)=> {
-      //        if(err){
-      //            console.log("Bledne zapytanie")
-      //        }
-      //        else{
-      //            console.log("Zwierzęta:", animalList)
-      //        }
-      //    })
+      console.log('Zwierzęta:', animalList);
     }
   });
 };
-// client.connect((err) => {
-//   if (err) {
-//     console.log('Błąd połączenia');
-//   } else {
-//     console.log('Połączenie udane');
-
-//     const db = client.db('test');
-
-//     const animals = db.collection('animals');
-
-//     // animals.insertOne(
-//     //   {
-//     //     nameAnimal: 'Kura',
-//     //     age: '45',
-//     //   },
-//     //   (err) => {
-//     //     if (err) {
-//     //       console.log('Nie dodano wpisu');
-//     //     } else {
-//     //       console.log('Wpis dodano');
-//     //     }
-//     //   }
-//     // );
-
-//     // clients.updateMany(
-//     //   {
-//     //     age: {
-//     //       $lt: 30,
-//     //     },
-//     //   },
-//     //   {
-//     //     $set: {
-//     //       active: true,
-//     //     },
-//     //   },
-//     //   (err) => {
-//     //     if (err) {
-//     //       console.log('Błąd Aktualizacji ;(');
-//     //     } else {
-//     //       console.log('Aktualizacja okej !');
-//     //     }
-//     //     client.close();
-//     //   }
-//     // );
-
-//    // animals.deleteOne({_id: mongo.ObjectID()});
-
-// //    animals.find({}).toArray((err,animalList)=> {
-// //        if(err){
-// //            console.log("Bledne zapytanie")
-// //        }
-// //        else{
-// //            console.log("Zwierzęta:", animalList)
-// //        }
-// //    })
-//   }
-// });
-
+printAnimalById = () => {
+  const printAnimalId = prompt('Jakie zwierze chcesz wyszukac: ');
+  animals.findOne({ _id: ObjectId(printAnimalId) }, (err, result) => {
+    if (err) throw err;
+    console.log('Nazwa: ' + result.nameAnimal + '  wiek:  ' + result.age);
+  });
+};
 action = (number) => {
   switch (parseInt(number)) {
     case 1:
-      //printData();
-      console.log('wybrales wyswietlenie bazy');
+      printAnimal();
       break;
     case 2:
-        addAnimal()
-        break;  
+      addAnimal();
+      break;
+    case 3:
+      // updateAnimal()
+      break;
+    case 4:
+      deleteAnimal();
+      break;
+    case 5:
+      printAnimalById();
+      break;
     case 7:
       console.log('Konczymy program');
       stopScript();
       break;
   }
 };
-connectDatabase();
-createMenu();
-const number = prompt('Twoj wybor to: ');
-action(number);
+
+var db;
+var animals;
+client.connect(() => {
+  db = client.db('test');
+  animals = db.collection('animals');
+  createMenu();
+  const number = prompt('Twoj wybor to: ');
+  action(number);
+});
